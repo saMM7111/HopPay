d12d9121ed7c639db72644f257530ee125d179c8
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-01 14:12:11 +0530

    add Account lookup by accountId

--- src/main/java/com/demo/hoppay/model/AccountRepository.java
+++ src/main/java/com/demo/hoppay/model/AccountRepository.java
@@ -2,5 +2,8 @@ package com.demo.hoppay.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, String> {
+	Optional<Account> findByAccountId(String accountId);
 }

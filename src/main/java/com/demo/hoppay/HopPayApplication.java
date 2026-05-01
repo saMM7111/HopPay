aa53ae36d3d8e13e4bf973b91564f0b2ccd1e7ce
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-01 12:52:09 +0530

    add Spring Boot entry point

---
+++ src/main/java/com/demo/hoppay/HopPayApplication.java
@@ -0,0 +1,11 @@
+package com.demo.hoppay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopPayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HopPayApplication.class, args);
+    }
+}

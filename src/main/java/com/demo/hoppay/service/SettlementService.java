b34619c7728240e270c85a56f7f4469aec4fef21
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-03 15:08:06 +0530

    wired repositories into SettlementService

--- src/main/java/com/demo/hoppay/service/SettlementService.java
+++ src/main/java/com/demo/hoppay/service/SettlementService.java
@@ -1,4 +1,16 @@
 package com.demo.hoppay.service;
 
+import com.demo.hoppay.model.AccountRepository;
+import com.demo.hoppay.model.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SettlementService {
+	private final AccountRepository accountRepository;
+	private final TransactionRepository transactionRepository;
+
+	public SettlementService(AccountRepository accountRepository, TransactionRepository transactionRepository) {
+		this.accountRepository = accountRepository;
+		this.transactionRepository = transactionRepository;
+	}
 }

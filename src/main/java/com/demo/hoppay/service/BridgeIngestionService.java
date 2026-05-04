fd6e6218a5963eeac736b90e2674d49ec7eb6b26
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-04 20:58:27 +0530

    wireed crypto + settlement into bridge service

--- src/main/java/com/demo/hoppay/service/BridgeIngestionService.java
+++ src/main/java/com/demo/hoppay/service/BridgeIngestionService.java
@@ -1,4 +1,15 @@
 package com.demo.hoppay.service;
 
+import com.demo.hoppay.crypto.HybridCryptoService;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BridgeIngestionService {
+	private final HybridCryptoService cryptoService;
+	private final SettlementService settlementService;
+
+	public BridgeIngestionService(HybridCryptoService cryptoService, SettlementService settlementService) {
+		this.cryptoService = cryptoService;
+		this.settlementService = settlementService;
+	}
 }

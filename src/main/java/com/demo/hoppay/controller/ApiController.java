cf23446856a06d36fa3c22fec987d651fdf79112
Sankalp Mehta <sammehta1000@gmail.com>
2026-05-08 11:46:05 +0530

    added API and dashboard controller skeletons

---
+++ src/main/java/com/demo/hoppay/controller/ApiController.java
@@ -0,0 +1,7 @@
+package com.demo.hoppay.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+}
---
+++ src/main/java/com/demo/hoppay/controller/DashboardController.java
@@ -0,0 +1,7 @@
+package com.demo.hoppay.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DashboardController {
+}
